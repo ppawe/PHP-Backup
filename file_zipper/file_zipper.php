1ddcf17d7ae4d21847c6f97f3cf60c1ea23c37e3
Fjermengard <54975867+Fjermengard@users.noreply.github.com>
2019-10-29 08:59:28 +0100

    Add files via upload

--- file_zipper/file_zipper.php
+++ file_zipper/file_zipper.php
@@ -1,13 +1,42 @@
 <?php
-$zip = new ZipArchive();
-$zip->open('file.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
-if ($zip->open('test.zip') === TRUE) {
-    $zip->addFile('/testfolder/file1.txt');
-    $zip->addFile('/testfolder/file2.txt');
-    $zip->addFile('/testfolder/file3.txt');
-    $zip->close();
-    echo 'ok';
-} else {
-    echo 'failed';
+ini_set('max_execution_time', 600);
+ini_set('memory_limit','1024M');
+
+zipData('testfolder', 'test_new.zip');
+echo "done, ";
+
+function zipData($source, $destination){
+  if(extension_loaded('zip') === true){
+    if(file_exists($source) === true){
+      $zip = new ZipArchive();
+      if($zip->open($destination, ZIPARCHIVE::CREATE) === true){
+        $source = realpath($source);
+        echo "realpath: ".$source."; ";
+
+        if(is_dir($source) === true) { //This should be the folder including the programm
+          $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+          var_dump($files);
+
+          foreach ($files as $file){
+            $file = realpath($file);
+
+            if(is_dir($file) === true){
+              $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            } else if(is_file($file) === true){
+              $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+          }
+        } else if(is_file($source) === true){
+          $zip->addFromString(basename($source), file_get_contents($source));
+        }
+      } else{
+        echo "couldnt open zip-Archive, ";
+      }
+      echo "the function was successfully run through, ";
+      return $zip->close();
+    }
+  }
+  echo "this didnt work, ";
+  return false;
 }
 ?>
