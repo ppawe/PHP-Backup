c8fd4f27dea62709e3b5fe97940c4a01894cd5c7
Fjermengard <54975867+Fjermengard@users.noreply.github.com>
2019-10-30 10:16:52 +0100

    Add files via upload

--- file_zipper/file_zipper.php
+++ file_zipper/file_zipper.php
@@ -2,7 +2,7 @@
 ini_set('max_execution_time', 600);
 ini_set('memory_limit','1024M');
 
-zipData('testfolder', 'test_new.zip');
+zipData('testfolder', 'test_new.zip'); //1st value is the folder to be zipped, 2nd value says where to place the zip-file (and its name)
 echo "done, ";
 
 function zipData($source, $destination){
@@ -11,22 +11,21 @@ function zipData($source, $destination){
       $zip = new ZipArchive();
       if($zip->open($destination, ZIPARCHIVE::CREATE) === true){
         $source = realpath($source);
-        echo "realpath: ".$source."; ";
+        echo "realpath: ".$source."; <br><br>";
 
         if(is_dir($source) === true) { //This should be the folder including the programm
           $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
-          var_dump($files);
 
           foreach ($files as $file){
+            echo $file . "<br>";
             $file = realpath($file);
 
-            if(is_dir($file) === true){
-              $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
-            } else if(is_file($file) === true){
-              $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            if(is_file($file) === true){
+              echo "it is a file <br>";
+              $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file)); //addFromString creates both the files as well as the directories they are placed into. Overwrites files of same name
             }
           }
-        } else if(is_file($source) === true){
+        } else if(is_file($source) === true){ //zip if its only a file
           $zip->addFromString(basename($source), file_get_contents($source));
         }
       } else{
@@ -39,4 +38,6 @@ function zipData($source, $destination){
   echo "this didnt work, ";
   return false;
 }
+
+//folder/subfolder/subfolder2/text.txt
 ?>
