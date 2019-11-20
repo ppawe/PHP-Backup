3d9febcf2f1c43cf3d4c9b776162674c4dcda34f
Kevin <kevin@test.de>
2019-11-20 09:30:48 +0100

    Administrator Modul eingebaut und zum laufen gebracht und ein paar unnötige Dateien gelöscht

---
+++ shopware_plugin/PluginBackupFilesAndDatabase/src/Resources/app/administration/src/module/backup-module/index.js
@@ -0,0 +1,30 @@
+import deDE from './snippet/de-DE.json';
+import enGB from './snippet/en-GB.json';
+
+Shopware.Module.register('backup-module', {
+    type: 'plugin',
+    name: 'Backup',
+    title: 'Backup module',
+    description: 'Ein Modul zum backupen von Shopware',
+    color: '#62ff80',
+    icon: 'default-object-lab-flask',
+
+    snippets: {
+        'de-DE': deDE,
+        'en-GB': enGB
+    },
+
+    routes: {
+        overview: {
+            component: 'sw-product-list',
+            path: 'overview'
+        },
+    },
+
+    navigation: [{
+        label: 'Backup',
+        color: '#62ff80',
+        path: 'backup.module.overview',
+        icon: 'default-object-lab-flask'
+    }]
+});
